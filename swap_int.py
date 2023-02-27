f35ed97d3db8d9384c8bd48bda1d7d6f06c7e3bf
Raudin <markmuga72@gmail.com>
2023-02-27 22:26:56 +0300

    Swap numbers without temp variable

---
+++ swap_int.py
@@ -0,0 +1,12 @@
+# Initialize two numbers
+num1 = 5
+num2 = 10
+
+# Swapping the numbers
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1 - num2
+
+# Print the swapped numbers
+print("num1 =", num1)
+print("num2 =", num2)
