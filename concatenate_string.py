643724dbec88b7e1ff4a346a05ae186dbbb255ab
Raudin <markmuga72@gmail.com>
2023-02-27 22:09:36 +0300

    concatenate 2 strings

---
+++ concatenate_string.py
@@ -0,0 +1,7 @@
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+
+# Using the + operator to concatenate two strings
+concatenated_str = str1 + str2
+
+print("The concatenated string is:", concatenated_str)
