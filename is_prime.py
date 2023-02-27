712645ae3f4513320bf897f5a2a95bad5942c16d
Raudin <markmuga72@gmail.com>
2023-02-27 22:24:28 +0300

    Check if prime number

---
+++ is_prime.py
@@ -0,0 +1,18 @@
+import math
+
+def is_prime(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    
+    return True
+
+# Example usage
+num = 17
+if is_prime(num):
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
